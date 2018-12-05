8a626cd4582a191bf4de040c2d879110d7814e83
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-12-05 14:36:23 +0100

    added Function to post to twitter

---
+++ src/postToTwitter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fabia_ovv7omp
+ * Date: 12/5/2018
+ * Time: 2:25 PM
+ */
+
+require_once ("config.php");
+require_once ("codebird/codebird.php");
+
+\Codebird\Codebird::setConsumerKey($API_KEY,$API_SECRET_KEY);
+
+$cd = \Codebird\Codebird::getInstance();
+
+$cd->setToken($ACCESS_TOKEN,$ACCESS_TOKEN_SECRET);
+
+$reply = $cd->statuses_update("status=Hello, World");
+
+echo $reply;
