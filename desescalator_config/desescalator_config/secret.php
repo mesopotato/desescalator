02de809b243668bbb694aca1e93ac599da2adf9c
mesopotato <dittrich.yannick@gmail.com>
2018-12-12 13:13:11 +0100

    Auth aber vielleicht unnötig

---
+++ desescalator_config/desescalator_config/secret.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Autor: Fabian Bienz
+ * Hinweis: Diese Datei darf nicht an Dritte weitergegeben werden. Sie enthalt Passwörter und Access-Token.
+ */
+
+/**
+ * Config File for Api Access Tokens and the like
+ * Folder desescalator_config shoul be in the same Directory as desescalator
+ */
+
+/**
+ * Twitter Account Details
+ * email:  dittrich.yannick@gmail.com
+ * pwd:  ^'09qwer
+ */
+
+ $secret = array(
+     "apikey" => "L4q6pYsH68gWK48EbX08Ky9av",
+     "apisecretkey" => "4rjJX7RQDPY3kxgv5UkHQVI6j2K0JANBGFWBWQ1kIkpIbVbUID",
+     "accesstoken" => "928752851265970177-8FtwvdyqN0cZtNXHfQGYx1FDrZFqMP7",
+     "accesstokensecret" => "ife34t6LiEo8uzZx9Gb5FLTIaxdP8kvSlYOliL7J3TD6l"
+ );
+
+ $secrettest = array(
+     "test" => "value"
+ );
+
+?>
--- index.php
+++ index.php
@@ -5,5 +5,33 @@
  * Date: 12/5/2018
  * Time: 1:39 PM
  */
+<?php
+session_start();
+require 'secret.php'; //??
 
-header("Location: public_html");
+use Abraham\TwitterOAuth\TwitterOAuth; //?? kommt das einfach woher??
+
+define('CONSUMER_KEY', 'L4q6pYsH68gWK48EbX08Ky9av'); // add consumer key 
+define('CONSUMER_SECRET', '4rjJX7RQDPY3kxgv5UkHQVI6j2K0JANBGFWBWQ1kIkpIbVbUID'); // add consumer secret 
+define('OAUTH_CALLBACK', 'http://localhost/desescalator2/callback.php'); //  app callback URL 
+
+
+if (!isset($_SESSION['access_token'])) {
+    
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+	$_SESSION['oauth_token'] = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+	
+    echo $url; // oder header oder sonzt was 
+    
+} else { // 
+    
+	$access_token = $_SESSION['access_token'];
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+	$user = $connection->get("account/verify_credentials");
+	echo $user->screen_name;
+}
+
+header("Location: public_html"); //wohin stellt sich heraus 
