ab643cc0f52b6eab88651a47fdfbbf04ad9786eb
mesopotato <dittrich.yannick@gmail.com>
2019-01-21 07:23:54 +0100

    codeCov rauf?

--- index.js
+++ index.js
@@ -189,8 +189,6 @@ function chat (msg, scan) {
         console.log('Scan is false and stream is stopped:' + scan + ' -----------------------------')
         stream.stop();
     }
-
-
 }
 
 //} else {
@@ -199,97 +197,6 @@ function chat (msg, scan) {
 //console.log('scan was falsescan was falsescan was falsescan was falsescan was falsescan was falsescan was falsescan was falsescan was false');
 //}
 //-----------------------------------------
-function isEmpty (obj) {
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key))
-            return false;
-    }
-    return true;
-}
 
-
-app.get('/ajaxcall', function (req, res) {
-    console.log('ajaxcall reached');
-    //res.write('<table style="width:100%">')
-
-
-    console.log('scanTwitter function reached');
-    var phrase = 'I hate';
-    var regex = /^I hate /;
-
-    var stream = T.stream('statuses/filter', { track: phrase })
-    stream.on('tweet', gotTweet);
-    function gotTweet(tweet) {
-
-        if (regex.test(tweet.text)) {
-            //   if (tweet.user.location != null){    
-
-            // res.send(' <tr> <th>'+tweet.text+'</th>  </tr>');
-            //response.end();
-            console.log(tweet.text);
-            // console.log(tweet)
-            console.log('------------------------------------------------------------');
-            //res.write(' <tr> <th>' + tweet.text + '</th>  </tr>');
-            //json = JSON.stringify(tweet.text) + '\n';
-            // json.stream.write(res);
-            //json.toStream().pipe(res);
-
-
-            //retweet line when necessary
-            T.post('statuses/retweet', { id: tweet.id_str }, retweeted);
-
-            function retweeted(err, data, response) {
-                if (err) {
-                    console.log("Error: " + err.message);
-                } else {
-                    console.log('Retweeted: ' + tweet.id);
-                }
-            }
-        }
-        // response.write('</table>');
-    }
-
-    var data = {
-        contactId: 1,
-        firstName: 'Jhon',
-        latName: 'Doe',
-        email: 'jhonDoe@email.com',
-        phone: '1234'
-    };
-
-    //res.send(data);
-});
-
-// Once every N milliseconds
-//setInterval(tweeter, 60*5*1000);
-
-function tweeter() {
-
-    // making diffenert 
-    var tweet = 'Here\'s a random number between 0 and 100: ' + Math.floor(Math.random() * 100);
-
-    // Post that tweet!
-    T.post('statuses/update', { status: tweet }, tweeted);
-
-    // Callback for when the tweet is sent
-    function tweeted(err, data, response) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('Success: ' + data.text);
-            //console.log(response);
-        }
-    };
-}
-
-function search1() {
-    T.get('search/tweets', {
-        q: 'banana since:2011-07-11'
-        , count: 100
-    }
-        , function (err, data, response) {
-            console.log(data)
-        });
-}
 
 module.exports = app; // for testing
