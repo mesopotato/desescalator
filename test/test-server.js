b3324afd951c168003bf9f79140cd5d9633400d0
mesopotato <dittrich.yannick@gmail.com>
2019-01-21 07:05:47 +0100

    test2

--- test/test-server.js
+++ test/test-server.js
@@ -10,3 +10,54 @@ describe('Array', function() {
   });
   
 });
+
+'use strict'
+
+var expect = require('chai').expect
+  , app = require('../index')
+  , io = require('socket.io-client')
+  , ioOptions = { 
+      transports: ['websocket']
+    , forceNew: true
+    , reconnection: false
+  }
+  , testMsg = {
+    string: "Donald Trump",
+    answer: "A answer",
+    checkbox: "off",
+    checkbox2: "off2"
+  }
+  , sender
+  , receiver
+
+describe('Socket Connection Test', function(){
+  beforeEach(function(done){
+    
+    // connect two io clients
+    sender = io('http://localhost:3000/', ioOptions)
+    receiver = io('http://localhost:3000/', ioOptions)
+    
+    // finish beforeEach setup
+    done()
+  })
+  afterEach(function(done){
+    
+    // disconnect io clients after each test
+    sender.disconnect()
+    receiver.disconnect()
+    done()
+  })
+
+  describe('Communication Server Client Test ', function(){
+
+    it('The CLient should recieve the Test Data when the Connect event is emited.', function(done){
+      sender.emit('connect', testMsg)
+      receiver.on('connected', function(msg){
+        expect(msg.firstName).to.equal("Jhon");
+        expect(msg.latName).to.equal("Doe");
+        expect(msg.email).to.equal("jhonDoe@email.com");
+        done()
+      })
+    })
+  })
+})
